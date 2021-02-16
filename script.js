9ad7c6157526fa30acbf22b3b8a288468af61de2
Hope Bradley <hope.e.bradley@gmail.com>
2021-02-16 16:38:44 -0500

    fixed colorchanger button

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-//alert('hellow');
+$(document).ready(function() {
+  const colorz = ['#EB9C9C', '#EBC49C', '#FFF287', '#B3E9D1', 
+    '#B3CBE9', '#CAC7FF', '#D9B0F3', '#FFC5EC'];
+  const lightColorz = ['#EB9C9C', '#EBC49C', '#FFF287', '#B3E9D1', 
+    '#B3CBE9', '#CAC7FF', '#D9B0F3', '#FFC5EC'];
 
-const colorz = ['#EB9C9C', '#EBC49C', '#FFF287', '#B3E9D1', '#B3CBE9', '#CAC7FF', '#D9B0F3', '#FFC5EC'];
-const lightColorz = ['#EB9C9C', '#EBC49C', '#FFF287', '#B3E9D1', '#B3CBE9', '#CAC7FF', '#D9B0F3', '#FFC5EC'];
+  //const elems = document.getElementsByClassName('colorchange');
 
-const paras = document.getElementsByClassName('paras');
-const colorChanger = document.getElementById('colorchanger');
-
-function colorChange() {
-	const color = colorz[Math.floor(Math.random()*colorz.length)];
-	const backgroundColor = lightColorz[Math.floor(Math.random()*lightColorz.length)];
-	
-	for (let i=0; i<paras.length; i++) {
-		paras[i].style.background = color;
-	}
-	document.body.style.background = backgroundColor; 
- }
-
-	colorChanger.addEventListener('click', colorChange);
+  const elems = $('.colorchange');
 
 
+  console.log(elems.length);
+  for (let i=0; i<elems.length; i++) {
+    console.log(elems[i]);
+  }
+
+
+  const colorChanger = document.getElementById('colorchanger');
+
+  function colorChange() {
+    const color = colorz[Math.floor(Math.random()*colorz.length)];
+    const backgroundColor = lightColorz[Math.floor(Math.random()*lightColorz.length)];
+    
+    for (let i=0; i<elems.length; i++) {
+      console.log(elems);
+      elems[i].style.background = color;
+    }
+    document.body.style.background = backgroundColor; 
+  }
+
+    colorChanger.addEventListener('click', colorChange);
+
+
+});

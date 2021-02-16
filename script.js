39630d9e2a4a3fddf90d1555897573f2ee886d9e
Hope Bradley <hope.e.bradley@gmail.com>
2021-02-15 20:03:21 -0500

    Added color-changing feature to body

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+//alert('hellow');
 
-const h2 = document.createElement('h2');
+const colorz = ['#EB9C9C', '#EBC49C', '#FFF287', '#B3E9D1', '#B3CBE9', '#CAC7FF', '#D9B0F3', '#FFC5EC'];
+const lightColorz = ['#EB9C9C', '#EBC49C', '#FFF287', '#B3E9D1', '#B3CBE9', '#CAC7FF', '#D9B0F3', '#FFC5EC'];
 
-h2.textContent = "This content added by JavaScript";
+const paras = document.getElementsByClassName('paras');
+
+function colorChange() {
+	const color = colorz[Math.floor(Math.random()*colorz.length)];
+	const backgroundColor = lightColorz[Math.floor(Math.random()*lightColorz.length)];
+	
+	for (let i=0; i<paras.length; i++) {
+		paras[i].style.background = color;
+	}
+	document.body.style.background = backgroundColor; 
+ }
+
+for (let i = 0; i<paras.length; i++) {
+	paras[i].addEventListener('click', colorChange);
+}
+
 
